e8648d48924fd0a609cf75250b1546f1dee2955c
Enrico Schumann <es@enricoschumann.net>
2023-02-14 10:28:47 +0100

    [vevent] Add argument "location"

--- R/functions.R
+++ R/functions.R
@@ -19,9 +19,9 @@
     "FR" = 5,
     "SA" = 6)
 
-.fold <- function(s) {
-    i <- nchar(s) > 75
-    ## TODO
+.fold <- function(s, max.bytes = 75) {
+    .NotYetImplemented(".fold")
+    i <- nchar(s, type = "bytes") > max.bytes
 }
 
 .unfold <- function(s, strict.eol) {
@@ -67,7 +67,7 @@ function(file,
     cal.txt <- .unfold(cal.txt, strict.eol)
 
 
-    ## TODO: text operations -- remove \, etc?
+    ## TODO text operations -- remove \, etc
     cal <- .properties(cal.txt, keep.source = keep.source)
     cal <- .expand_properties(cal, tz.names = tz.names)
 
@@ -1091,6 +1091,7 @@ function(dtstart,
          description = NULL,
          uid = NULL,
          categories = NULL,
+         location = NULL,
          ...,
          vcalendar = TRUE,
          file,
@@ -1116,7 +1117,7 @@ function(dtstart,
            else
                uid
 
-    events <- character(0L)
+    events <- vector("list", len)
     for (i in seq_len(len)) {
         event <- c(paste0("UID:", UID[i]),
                    paste0("SUMMARY:", summary[i]),
@@ -1138,13 +1139,19 @@ function(dtstart,
             event <- c(event,
                        paste0("DTEND:", DTEND))
         }
-        event <- c(event,
-                   paste0("CATEGORIES:",
-                          paste0(categories, collapse = ",")))
+        if (!is.null(categories))
+            event <- c(event,
+                       paste0("CATEGORIES:",
+                              paste0(categories, collapse = ",")))
+        if (!is.null(location))
+            event <- c(event,
+                       paste0("LOCATION:",
+                              paste0(location, collapse = ",")))
         event <- c("BEGIN:VEVENT",
                    event,
                    "END:VEVENT")
-        events <- c(events, event)
+        class(event) <- "vevent"
+        events[[i]] <- event
     }
 
     if (vcalendar) {
@@ -1165,7 +1172,7 @@ function(dtstart,
     if (!missing(file)) {
         ## TODO FOLD
 
-        writeLines(paste(paste0(events, .eol.strict), collapse = ""), file)
+        writeLines(paste(paste0(unlist(events), .eol.strict), collapse = ""), file)
         invisible(events)
     } else
         events
---
+++ R/utilities.R
@@ -0,0 +1,79 @@
+## copied from datetimeutils https://github.com/enricoschumann/datetimeutils
+
+.mday <- function(x)
+    as.POSIXlt(x)$mday
+
+.year <- function(x)
+    as.POSIXlt(x)$year + 1900
+
+.month <- function(x)
+    as.POSIXlt(x)$mon + 1
+
+.week <- function(x, WKST = 1) {
+
+    ## RFC 5545, 3.3.10.
+    ## 
+    ## Week number one of the calendar year is the first
+    ## week that contains at least four (4) days in that
+    ## calendar year.
+
+    if (!inherits(x, "Date"))
+        x <- as.Date(as.POSIXlt(x))
+
+    x <- (x + 4 - ((.weekday(x) - WKST) %% 7 +1))
+    y1 <- x - as.POSIXlt(x)$yday
+    as.numeric(x - y1) %/% 7+1
+}
+
+.start_of_week <- function(x, WKST = 1, Date = TRUE) {
+
+    ## map a given date to the start of its week
+    offset <- if (WKST == 0)
+                  3
+              else
+                  WKST - 4
+
+    if (Date)
+        .Date(7 * (unclass(x - offset) %/% 7) + offset)
+    else
+              7 * (unclass(x - offset) %/% 7) + offset
+}
+
+.weekday <- function(x)
+    ## maps Date to a number (0 Sunday .. 6 Saturday)
+    unclass(x + 4) %% 7
+
+.wday <- c(
+    "SU" = 0,
+    "MO" = 1,
+    "TU" = 2,
+    "WE" = 3,
+    "TH" = 4,
+    "FR" = 5,
+    "SA" = 6)
+
+.weekdayS <- function(dates)
+        c("SU",
+          "MO",
+          "TU",
+          "WE",
+          "TH",
+          "FR",
+          "SA")[unclass(dates + 4) %% 7 + 1]
+
+.next_weekday <- function(wday, start, count = 1, interval = 1) {
+    ## wday : 0 SU, 1 MO ... => target day
+    ## start: a date
+    if (count == 0)
+        return(numeric(0L))
+    ans <- (wday - .weekday(start)) %% 7 +
+        rep(7 * interval * seq(0, count - 1), each = length(wday))
+    ans <- sort(ans) + start
+    ans[seq_len(count)]
+}
+
+.date2char <- function(d)
+    format(d, format = "%Y%m%d")
+
+.char2date <- function(s)
+    as.Date(s, format = "%Y%m%d")
